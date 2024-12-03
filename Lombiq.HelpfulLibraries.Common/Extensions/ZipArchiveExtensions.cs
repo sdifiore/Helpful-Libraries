10f42584993d7102fbf453d8e275079952fd9408
Sára El-Saig <sara.el-saig@lombiq.com>
2024-12-03 11:59:57 +0100

    Add ZipArchiveExtensions.

---
+++ Lombiq.HelpfulLibraries.Common/Extensions/ZipArchiveExtensions.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+
+namespace Lombiq.HelpfulLibraries.Common.Extensions;
+
+public static class ZipArchiveExtensions
+{
+    /// <summary>
+    /// Creates a new text file in <paramref name="zip"/> and writes the <paramref name="lines"/> into it.
+    /// </summary>
+    public static async Task CreateTextEntryAsync(ZipArchive zip, string entryName, IEnumerable<string> lines)
+    {
+        await using var writer = new StreamWriter(zip.CreateEntry(entryName).Open());
+
+        foreach (var line in lines)
+        {
+            await writer.WriteLineAsync(line);
+        }
+    }
+
+    /// <summary>
+    /// Creates a new binary file in <paramref name="zip"/> and writes the <paramref name="data"/> into it.
+    /// </summary>
+    public static async Task CreateBinaryEntryAsync(ZipArchive zip, string entryName, byte[] data)
+    {
+        await using var stream = zip.CreateEntry(entryName).Open();
+        await stream.WriteAsync(data);
+    }
+}
