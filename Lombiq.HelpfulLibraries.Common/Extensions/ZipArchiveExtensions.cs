4328568e43220a47a0a7cd49ad0b7bfc083cfb54
Sára El-Saig <sara.el-saig@lombiq.com>
2024-12-03 12:04:27 +0100

    Add single line overload.

--- Lombiq.HelpfulLibraries.Common/Extensions/ZipArchiveExtensions.cs
+++ Lombiq.HelpfulLibraries.Common/Extensions/ZipArchiveExtensions.cs
@@ -8,7 +8,7 @@ public static class ZipArchiveExtensions
     /// <summary>
     /// Creates a new text file in <paramref name="zip"/> and writes the <paramref name="lines"/> into it.
     /// </summary>
-    public static async Task CreateTextEntryAsync(ZipArchive zip, string entryName, IEnumerable<string> lines)
+    public static async Task CreateTextEntryAsync(this ZipArchive zip, string entryName, IEnumerable<string> lines)
     {
         await using var writer = new StreamWriter(zip.CreateEntry(entryName).Open());
 
@@ -19,9 +19,15 @@ public static class ZipArchiveExtensions
     }
 
     /// <summary>
+    /// Creates a new text file in <paramref name="zip"/> and writes the <paramref name="text"/> into it.
+    /// </summary>
+    public static Task CreateTextEntryAsync(this ZipArchive zip, string entryName, string text) =>
+        zip.CreateTextEntryAsync(entryName, [text]);
+
+    /// <summary>
     /// Creates a new binary file in <paramref name="zip"/> and writes the <paramref name="data"/> into it.
     /// </summary>
-    public static async Task CreateBinaryEntryAsync(ZipArchive zip, string entryName, byte[] data)
+    public static async Task CreateBinaryEntryAsync(this ZipArchive zip, string entryName, byte[] data)
     {
         await using var stream = zip.CreateEntry(entryName).Open();
         await stream.WriteAsync(data);
